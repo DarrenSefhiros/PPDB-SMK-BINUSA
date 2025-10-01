fcecf4ab7247174f30c9828ca2d0009bf5567789
darren <d.sefh007@gmail.com>
2025-10-01 14:59:50 +0700

    projek ppdb

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  // Import yang benar
+import Homepage from './Components/Homepage';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Homepage = () => {
+  return (
+    <div className="box1 bg-sky-500">
+      <ul className="flex items-center justify-between"> 
+        <h1 className="text-sky-600">
+          Home
+      </h1>
+        <li>
+          Login
+        </li>
+        <li className="mr-100">
+          Tentang Kami
+        </li>
+      </ul>
+      <h1>hhellaw</h1>
+    </div>
+  )
+}
+
+export default Homepage
